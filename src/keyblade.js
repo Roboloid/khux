a89036745843e850b7bb61d6b4e9eb4cad6ec382
Roboloid <roboloidx@gmail.com>
2020-01-04 16:14:24 -0500

    Button progress

--- src/keyblade.js
+++ src/keyblade.js
@@ -1,20 +1,12 @@
+// Toggle dropdown
 function keybladeDropdown() {
   document.getElementById("dropdown_container").classList.toggle("show");
 }
 
-function filterFunction() {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("searchInput");
-  filter = input.value.toUpperCase();
-  div = document.getElementById("dropdown_container");
-  a = div.getElementsByTagName("a");
-  for (i = 0; i < a.length; i++) {
-    txtValue = a[i].textContent || a[i].innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      a[i].style.display = "";
-    } else {
-      a[i].style.display = "none";
-    }
+function selectKeyblade(value)  {
+  if(value != "") {
+    console.log(value);
+    window.alert(value);
   }
 }
 
@@ -30,4 +22,4 @@ window.onclick = function(event) {
       }
     }
   }
-} 
+}
