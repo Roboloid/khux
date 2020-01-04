85fae3c680e8f26c16eccbea03dd55c25aa7eccd
Roboloid <roboloidx@gmail.com>
2020-01-04 11:58:16 -0500

    Testind dropdown

--- src/keyblade.js
+++ src/keyblade.js
@@ -2,6 +2,22 @@ function keybladeDropdown() {
   document.getElementById("dropdown_container").classList.toggle("show");
 }
 
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("searchInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("dropdown_container");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
+
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
