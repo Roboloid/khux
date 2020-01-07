b785e160030034ce593916dadde2d9ff111498f8
Roboloid <roboloidx@gmail.com>
2020-01-06 22:49:14 -0500

    Updated number support (beta)

--- src/keyblade.js
+++ src/keyblade.js
@@ -15,11 +15,22 @@ function selectKeyblade(value)  {
     document.getElementById("level1").value = "";
     document.getElementById("level2").value = "";
     currentKeyblade = value;
+    grabKeybladeImg(currentKeyblade);
     cost1 = "";
     cost2 = "";
 
+    document.getElementById("showPower").classList.toggle("showPowerNum", false);
+    document.getElementById("showSpeed").classList.toggle("showSpeedNum", false);
+    document.getElementById("showMagic").classList.toggle("showMagicNum", false);
+    document.getElementById("showSun").classList.toggle("showSunNum", false);
+    document.getElementById("showMoon").classList.toggle("showMoonNum", false);
+
     document.getElementById("number_container").classList.toggle("showNum", true);
   }
+}
+
+function grabKeybladeImg(value)  {
+  document.getElementById("kbImage").src = "./public/images/keyblade/" + value + "_3.png";
 }
 
 function inputValue(value)  {
@@ -30,7 +41,7 @@ function inputValue(value)  {
     cost1 = parseFloat(value);
   }
   calculateCost();
-  document.getElementById("displayValue_container").innerHTML = value;
+  // document.getElementById("displayValue_container").innerHTML = value;
 }
 
 function inputValue2(value)  {
@@ -41,19 +52,132 @@ function inputValue2(value)  {
     cost2 = parseFloat(value);
   }
   calculateCost();
-  document.getElementById("displayValue_container2").innerHTML = value;
+  // document.getElementById("displayValue_container2").innerHTML = value;
+}
+
+function sumCost(keyblade, base, target) {
+  var sum = 0;
+  for(var value in keyblade)  {
+    if(value <= base || value > target) {
+
+    }
+    else  {
+      sum = sum + keyblade[value];
+    }
+  }
+  return sum;
 }
 
 function calculateCost() {
   if(cost1 == "NaN" || cost2 == "NaN" || currentKeyblade == "")  { // Inputs not yet full, do nothing
-    // document.getElementById("displayValue_container3").innerHTML = "Nope.";
+    document.getElementById("displayValue_container3").innerHTML = "";
   }
   else  {
-    if(cost1 >= cost2)  { // Inputs full but invalid (smaller level is not smaller)
-      // document.getElementById("displayValue_container3").innerHTML = "Almost." + cost1 + " " + cost2;
+    if(cost1 >= cost2 || cost1 < 0 || cost2 > 50)  { // Inputs full but invalid
+      document.getElementById("displayValue_container3").innerHTML = "";
+        document.getElementById("showPower").classList.toggle("showPowerNum", false);
+        document.getElementById("showSpeed").classList.toggle("showSpeedNum", false);
+        document.getElementById("showMagic").classList.toggle("showMagicNum", false);
+        document.getElementById("showSun").classList.toggle("showSunNum", false);
+        document.getElementById("showMoon").classList.toggle("showMoonNum", false);
     }
     else  {
       // document.getElementById("displayValue_container3").innerHTML = "Success!" + cost1 + " " + cost2;
+      var thisBlade;
+      switch(currentKeyblade) {
+        case "kb1":
+          thisBlade = kb1_cost;
+          break;
+        case "kb2":
+          thisBlade = kb2_cost;
+          break;
+        case "kb3":
+          thisBlade = kb3_cost;
+          break;
+        case "kb4":
+          thisBlade = kb4_cost;
+          break;
+        case "kb5":
+          thisBlade = kb5_cost;
+          break;
+        case "kb6":
+          thisBlade = kb6_cost;
+          break;
+        case "kb7":
+          thisBlade = kb7_cost;
+          break;
+        case "kb8":
+          thisBlade = kb8_cost;
+          break;
+        case "kb9":
+          thisBlade = kb9_cost;
+          break;
+        case "kb10":
+          thisBlade = kb10_cost;
+          break;
+        case "kb11":
+          thisBlade = kb11_cost;
+          break;
+        case "kb12":
+          thisBlade = kb12_cost;
+          break;
+        case "kb13":
+          thisBlade = kb13_cost;
+          break;
+        case "kb14":
+          thisBlade = kb14_cost;
+          break;
+        case "kb15":
+          thisBlade = kb15_cost;
+          break;
+        case "kb16":
+          thisBlade = kb16_cost;
+          break;
+      }
+      var powerCost = sumCost(thisBlade.power, cost1, cost2);
+      var speedCost = sumCost(thisBlade.speed, cost1, cost2);
+      var magicCost = sumCost(thisBlade.magic, cost1, cost2);
+      var sunCost = sumCost(thisBlade.sun, cost1, cost2);
+      var moonCost = sumCost(thisBlade.moon, cost1, cost2);
+      if(powerCost != 0)  {
+        document.getElementById("showPower").classList.toggle("showPowerNum", true);
+        document.getElementById("powerCost_container").innerHTML = powerCost;
+      }
+      else  {
+        document.getElementById("showPower").classList.toggle("showPowerNum", false);
+      }
+
+      if(speedCost != 0)  {
+        document.getElementById("showSpeed").classList.toggle("showSpeedNum", true);
+        document.getElementById("speedCost_container").innerHTML = speedCost;
+      }
+      else  {
+        document.getElementById("showSpeed").classList.toggle("showSpeedNum", false);
+      }
+
+      if(magicCost != 0)  {
+        document.getElementById("showMagic").classList.toggle("showMagicNum", true);
+        document.getElementById("magicCost_container").innerHTML = magicCost;
+      }
+      else  {
+        document.getElementById("showMagic").classList.toggle("showMagicNum", false);
+      }
+
+      if(sunCost != 0)  {
+        document.getElementById("showSun").classList.toggle("showSunNum", true);
+        document.getElementById("sunCost_container").innerHTML = sunCost;
+      }
+      else  {
+        document.getElementById("showSun").classList.toggle("showSunNum", false);
+      }
+
+      if(moonCost != 0)  {
+        document.getElementById("showMoon").classList.toggle("showMoonNum", true);
+        document.getElementById("moonCost_container").innerHTML = moonCost;
+      }
+      else  {
+        document.getElementById("showMoon").classList.toggle("showMoonNum", false);
+      }
       
     }
   }
@@ -72,3 +196,73 @@ window.onclick = function(event) {
     }
   }
 }
+
+var kb1_cost = {      // Keyblade1: Starlight
+  power:  {
+    25: 0,
+    26: 0,
+    27: 1,
+    28: 0,
+    29: 1,
+    30: 1,
+    31: 2,
+    32: 1,
+    33: 2,
+    34: 3,
+    35: 3
+  },
+  speed:  {
+    25: 0,
+    26: 1,
+    27: 0,
+    28: 1,
+    29: 1,
+    30: 1,
+    31: 1,
+    32: 2,
+    33: 2,
+    34: 3,
+    35: 3
+  },
+  magic:  {
+    25: 0,
+    26: 0,
+    27: 1,
+    28: 1,
+    29: 1,
+    30: 1,
+    31: 1,
+    32: 2,
+    33: 2,
+    34: 2,
+    35: 4
+  },
+  sun:  {
+    40: 1,
+    41: 1,
+    42: 1,
+    43: 1,
+    44: 1,
+    45: 1,
+    46: 1,
+    47: 1,
+    48: 1,
+    49: 1,
+    50: 1
+  },
+  moon: {
+    40: 1,
+    41: 1,
+    42: 1,
+    43: 1,
+    44: 1,
+    45: 1,
+    46: 1,
+    47: 1,
+    48: 1,
+    49: 1,
+    50: 1
+  }
+};      // Keyblade1: Starlight
+
+
