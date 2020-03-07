6afd54c53568c7e3a6f620568ecda03d82b88425
Roboloid <roboloidx@gmail.com>
2020-03-06 21:37:16 -0500

    Added a display all button

--- src/maps.js
+++ src/maps.js
@@ -1,5 +1,23 @@
 // Display maps
 function mapFunction(value)  {
-  var variable = value + "_Container";
-  document.getElementById(variable).classList.toggle("showMap");
+  if(value == "All")  {
+    document.getElementById("Daybreak_Town_Container").classList.toggle("showMap");
+    document.getElementById("Dwarf_Woodlands_Container").classList.toggle("showMap");
+    document.getElementById("Wonderland_Container").classList.toggle("showMap");
+    document.getElementById("Agrabah_Container").classList.toggle("showMap");
+    document.getElementById("Olympus_Coliseum_Container").classList.toggle("showMap");
+    document.getElementById("Beast's_Castle_Container").classList.toggle("showMap");
+    document.getElementById("Castle_of_Dreams_Container").classList.toggle("showMap");
+    document.getElementById("Enchanted_Dominion_Container").classList.toggle("showMap");
+    document.getElementById("Game_Central_Station_Container").classList.toggle("showMap");
+    document.getElementById("Niceland_Container").classList.toggle("showMap");
+    document.getElementById("Cy-Bug_Sector_Container").classList.toggle("showMap");
+    document.getElementById("Candy_Kingdom_Container").classList.toggle("showMap");
+    document.getElementById("Extras_Container").classList.toggle("showMap");
+  }
+  else  {
+    var variable = value + "_Container";
+    document.getElementById(variable).classList.toggle("showMap");
+  }
+  
 }
